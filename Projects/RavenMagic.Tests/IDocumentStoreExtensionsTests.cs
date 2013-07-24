da5105863b16e4cd5ca06942fad6f2470fc8c65e
Tim Murphy <tim@26tp.com>
2013-07-24 11:54:49 +1000

    New IDocumentStoreExtensions.QueryCollections().

---
+++ Projects/RavenMagic.Tests/IDocumentStoreExtensionsTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Raven.Client;
+using Raven.Client.Embedded;
+using RavenMagic.Tests.TestHelpers.Models;
+
+namespace RavenMagic.Tests
+{
+    public class IDocumentStoreExtensionsTests
+    {
+        [TestClass]
+        public class QueryCollections
+        {
+            [TestMethod]
+            public void ShouldThrowArgumentNullExceptionWhen_documentStore_IsNull()
+            {
+                // When
+                Action action = () => IDocumentStoreExtensions.QueryCollections(null);
+
+                // Then
+                action
+                    .ShouldThrow<ArgumentNullException>()
+                    .WithMessage("Value cannot be null.\r\nParameter name: documentStore");
+            }
+
+            [TestMethod]
+            public void ShouldReturnEnumerableOfCollectionsInDocumentStore()
+            {
+                // Given
+                IDocumentStore store = (new EmbeddableDocumentStore() { RunInMemory = true }).Initialize();
+                
+                using (IDocumentSession session = store.OpenSession())
+                {
+                    session.Store(new Person());
+                    session.Store(new Product());
+
+                    session.SaveChanges();
+                }
+
+                // When
+                var collections = store.QueryCollections();
+
+                // Then
+                collections.Should().BeEquivalentTo(new string[] {"People", "Products"}, "because we have added model types Person & Product to the database");
+            }
+
+            [TestMethod]
+            public void ShouldReturnEmptyEnumerableWhenDocumentStoreIsEmpty()
+            {
+                // Given
+                IDocumentStore store = (new EmbeddableDocumentStore() { RunInMemory = true }).Initialize();
+
+                // When
+                var collections = store.QueryCollections();
+
+                // Then
+                collections.Any().Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ Projects/RavenMagic/IDocumentStoreExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Raven.Client;
+using OpenMagic;
+using Raven.Client.Indexes;
+using Raven.Client.Document;
+
+namespace RavenMagic
+{
+    public static class IDocumentStoreExtensions
+    {
+        private static void CreateDocumentsByEntityNameIndex(this IDocumentStore documentStore, bool waitForNonStaleResults)
+        {
+            var documentsIndex = new RavenDocumentsByEntityName();
+
+            // Ensure the required index exists.
+            documentStore.ExecuteIndex(documentsIndex);
+
+            // Wait for indexing to finish.
+            if (waitForNonStaleResults)
+            {
+                using (IDocumentSession session = documentStore.OpenSession())
+                {
+                    var query = session.Query<object>(documentsIndex.IndexName).Customize(x => x.WaitForNonStaleResults());
+                    var result = query.FirstOrDefault();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Query <see cref="IDocumentStore"/> for the collections it holds.
+        /// </summary>
+        /// <param name="documentStore">The document store to query.</param>
+        /// <exception cref="System.ArgumentNullException">When <paramref name="documentStore"/> is null.</exception>
+        /// <remarks>
+        /// Method does not support more than 1024 collections. If you have more than 1024 collections you don't need this library :-)
+        /// </remarks>
+        public static IEnumerable<string> QueryCollections(this IDocumentStore documentStore)
+        {
+            documentStore.MustNotBeNull("documentStore");
+
+            documentStore.CreateDocumentsByEntityNameIndex(waitForNonStaleResults: true);
+
+            // Get the collection names.
+            var collections = documentStore.DatabaseCommands.GetTerms("Raven/DocumentsByEntityName", "Tag", "", 1024);
+
+            return collections;
+        }
+    }
+}
